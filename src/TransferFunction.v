e4e20536d24dc593554e58fa212a9c078ad66c89
mfehr <mathieu.fehr@gmail.com>
2020-11-29 00:05:31 +0100

    [theory] Add transfer function class definition

---
+++ src/TransferFunction.v
@@ -0,0 +1,19 @@
+From PresburgerAI Require Export RelationalAbstractDomain Imp.
+
+Section TransferFunctionDefinition.
+
+  Context {AbstractState: Type}
+          {adom: AbstractDomain (State * State) AbstractState}
+          (AR: RelationalAbstractDomain adom).
+
+  (** Properties on abstract domain to be able to use the abstract interpreter
+      for Imp **)
+  Class TransferFunction :=
+  {
+    transfer_binop : Vid -> BinOpCode -> Vid -> Vid -> AbstractState;
+    transfer_binop_sound :
+      forall res opc op1 op2 s s', s =[ PBinop res opc op1 op2 ]=> s' ->
+      in_dom (s, s') (transfer_binop res opc op1 op2)
+  }.
+
+End TransferFunctionDefinition.
