274f5b08377254b3a692e47b39875dff3a28471e
mfehr <mathieu.fehr@gmail.com>
2020-11-28 23:14:35 +0100

    [theory] Change a bit AbstractDomain

--- src/AbstractDomain.v
+++ src/AbstractDomain.v
@@ -8,14 +8,16 @@ Class AbstractDomain (ConcreteType AbstractType: Type) :=
   join : AbstractType -> AbstractType -> AbstractType;
 
   gamma : AbstractType -> Ensemble ConcreteType;
-  le a1 a2 := Included _ (gamma a1) (gamma a2);
+  in_dom c a := Ensembles.In _ (gamma a) c;
+  le a1 a2 := forall x, in_dom x a1 -> in_dom x a2;
 
-  gamma_top : forall x, Ensembles.In ConcreteType (gamma top) x;
-  gamma_bot : forall x, ~ Ensembles.In ConcreteType (gamma bot) x;
-  join_sound_l : forall a1 a2, Included _ (gamma a1) (gamma (join a1 a2));
-  join_sound_r : forall a1 a2, Included _ (gamma a2) (gamma (join a1 a2));
+  gamma_top : forall x, in_dom x top;
+  gamma_bot : forall x, ~ in_dom x bot;
+  join_sound_l : forall a1 a2, le a1 (join a1 a2);
+  join_sound_r : forall a1 a2, le a2 (join a1 a2);
 }.
 
+(** Some simple facts about abstract domains **)
 Section AbstractDomainTheorems.
 
   Context {ConcreteType AbstractType: Type}
@@ -24,39 +26,34 @@ Section AbstractDomainTheorems.
   Theorem le_refl :
     forall a, le a a.
   Proof.
-    unfold le, Included.
+    unfold le.
     auto.
   Qed.
 
   Theorem le_trans :
     forall a1 a2, le a1 a2 -> forall a3, le a2 a3 -> le a1 a3.
   Proof.
-    unfold le, Included.
+    intros.
+    unfold le in *.
     auto.
   Qed.
 
   Theorem le_join_l :
     forall a1 a2, le a1 a2 -> forall a3, le a1 (join a2 a3).
   Proof.
-    unfold le, Included.
-    intros.
-    apply join_sound_l.
-    auto.
+    eauto using le_trans, join_sound_l.
   Qed.
 
   Theorem le_join_r :
     forall a1 a2, le a1 a2 -> forall a3, le a1 (join a3 a2).
   Proof.
-    unfold le, Included.
-    intros.
-    apply join_sound_r.
-    auto.
+    eauto using le_trans, join_sound_r.
   Qed.
 
   Theorem le_bot :
     forall a, le bot a.
   Proof.
-    unfold le, Included.
+    unfold le.
     intros.
     apply gamma_bot in H.
     inversion H.
